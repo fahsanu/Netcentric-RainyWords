3edc65b79ff29ec4d455e78d5db45ef48dcfcfd6
Thenampetch <punpaporn.saa@gmail.com>
2023-11-07 16:13:40 +0700

    Change Button and Add Name in gamePage

--- client/src/app/welcomePage/button.tsx
+++ client/src/app/welcomePage/button.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React from "react";
 
 type ButtonProps = {
-    onClick: () => void;
-    isActive: boolean;
-    mode: string;
-    children: React.ReactNode;
-  };
-  
-  const Button: React.FC<ButtonProps> = ({ onClick, isActive, children }) => {
-    const buttonClasses = isActive
-      ? 'bg-amber-300'
-      : 'bg-stone-300';
-  
-    return (
-      <button
-        onClick={onClick}
-        className={`px-20 py-4 my-8 text-black text-4xl font-bold ${buttonClasses} border-4 border-black hover:bg-amber-300`}
-      >
-        {children}
-      </button>
-    );
-  };
+  onClick: () => void;
+  isActive: boolean;
+  mode: string;
+  children: React.ReactNode;
+};
+
+const Button: React.FC<ButtonProps> = ({ onClick, isActive, children }) => {
+  const buttonClasses = isActive ? "bg-amber-300" : "bg-stone-300";
+
+  return (
+    <button
+      onClick={onClick}
+      className={`px-20 py-4 my-8 text-black text-4xl font-bold ${buttonClasses} border-4 border-black hover:bg-amber-300`}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
